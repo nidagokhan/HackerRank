257c5a18b2647b08769f1bff6f7e6abfb560abbe
ndgkhn <nida.gokhan@gmail.com>
2023-10-08 11:17:44 +0300

    .

--- HackerRank.Sample02/Program.cs
+++ HackerRank.Sample02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,30 +11,43 @@ namespace HackerRank.Sample02
     {
         static void Main(string[] args)
         {
-            int m = 4;
-            List<int> list = new List<int>() {2,5,4,3};
-            int[] index = new int[1];
-            index[0] = 1;
+            #region İcecream Parlor Test
+            //int m = 4;
+            //List<int> list = new List<int>() { 2, 5, 4, 3 };
+            //int[] index = new int[1];
+            //index[0] = 1;
 
-            for (int i = 0; i < list.Count; i++)
-            {
-                if ((list[0] + list[i]) == m)
-                {
-                    Array.Resize(ref index, index.Length + 1);
-                    index[index.Length-1] = i+1;
-                }
-                //else if (list[0] == list[i])
-                //{
-                //    list[0] = 0;
-                //    Array.Resize(ref index, index.Length + 1);
-                //    index[index.Length - 1] = i + 1;
-                //}
-            }
+            //for (int i = 0; i < list.Count; i++)
+            //{
+            //    if ((list[0] + list[i]) == m)
+            //    {
+            //        Array.Resize(ref index, index.Length + 1);
+            //        index[index.Length - 1] = i + 1;
+            //    }
+            //    //else if (list[0] == list[i])
+            //    //{
+            //    //    list[0] = 0;
+            //    //    Array.Resize(ref index, index.Length + 1);
+            //    //    index[index.Length - 1] = i + 1;
+            //    //}
+            //}
 
-            foreach (int i in index)
-            {
-                Console.WriteLine(i);
-            }
+            //foreach (int i in index)
+            //{
+            //    Console.WriteLine(i);
+            //} 
+            #endregion
+
+            #region Listeyi/diziyi tersten yazdırma
+            //List<int> list = new List<int>() { 1, 4, 3, 2 };
+            //list.Reverse();
+            //foreach (int i in list)
+            //{
+            //    Console.Write(i+" ");
+            //} 
+            #endregion
+
+
             Console.ReadKey();
         }
 
@@ -41,5 +55,48 @@ namespace HackerRank.Sample02
         //{
 
         //}
+
+        #region Repeated String
+        //public static long repeatedString(string s, long n)
+        //{
+        //    #region Optimize edilmiş hali
+        //    //int length = s.Length;
+        //    //long fullRepeats = n / length;
+        //    //int remainingChars = (int)(n % length);
+
+        //    //long count_a = s.Count(c => c == 'a');
+
+        //    //long totalCount = fullRepeats * count_a + s.Substring(0, remainingChars).Count(c => c == 'a');
+
+        //    //return totalCount;
+        //    #endregion
+
+        //    char[] chars = s.ToCharArray();
+        //    long count_a = 0;
+
+        //    if (s == "a" && chars.Length == 1)
+        //    {
+        //        count_a = n;
+        //    }
+        //    else
+        //    {
+        //        for (int i = 0; i < n; i++)
+        //        {
+        //            if (chars.Length != n)
+        //            {
+        //                Array.Resize(ref chars, chars.Length + 1);
+        //                chars[chars.Length - 1] = chars[i];
+        //            }
+        //        }
+
+        //        foreach (char c in chars)
+        //        {
+        //            if (c == 'a')
+        //                count_a++;
+        //        }
+        //    }
+        //    return count_a;
+        //} 
+        #endregion
     }
 }
