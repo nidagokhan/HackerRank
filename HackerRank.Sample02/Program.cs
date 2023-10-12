cd80a6d5bb712e93afb5573fb7c2de76bbe8a32b
ndgkhn <nida.gokhan@gmail.com>
2023-10-12 03:18:04 +0300

    new solution added

--- HackerRank.Sample02/Program.cs
+++ HackerRank.Sample02/Program.cs
@@ -126,39 +126,103 @@ namespace HackerRank.Sample02
             //} 
             #endregion
 
-            int sonuc = beautifulDays(13,45,3);
-            Console.WriteLine(sonuc);
+            #region MyRegion
+            //int sonuc = beautifulDays(13,45,3);
+            //Console.WriteLine(sonuc); 
+            #endregion
+
+            List<int> list = new List<int>() { 19, 10, 12, 10, 24, 25, 22 };
+            int k = 4;
+            int toplam = 0;
+            int result = 0;
+            List<int> son = new List<int>();
+
+            #region 1.yol
+            //for (int i = 1; i < list.Count; i++)
+            //{
+            //    toplam= list[0] + list[i];
+            //    if (toplam % k!=0)
+            //    {
+            //        son.Add(list[i]);
+            //        son.Add(list[0]);
+            //    }
+            //} 
+            #endregion
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 0; j < list.Count; j++)
+                {
+                    toplam = list[i] + list[j];
+                    if (toplam % k != 0)
+                    {
+                        son.Add(list[i]);
+                        son.Add(list[j]);
+                    }
+                }
+            }
+            son = son.Distinct().ToList();
+            result = son.Count();
+            Console.WriteLine(result);
             Console.ReadKey();
         }
 
-        public static int beautifulDays(int i, int j, int k)
-        {
-            int start = i;
-            int finish = j;
-            int result = 0;
-            int count = 0;
+        #region beautifulDays
+        //public static int beautifulDays(int i, int j, int k)
+        //{
+        //    int start = i;
+        //    int finish = j;
+        //    int result = 0;
+        //    int count = 0;
 
-            for (int a = start; a <= finish; a++)
-            {
-                result = Math.Abs(a - Ters(a));
-                if (result % k == 0)
-                {
-                    count++;
-                }
-            }
-            return count;
-        }
-        public static int Ters(int sayi)
-        {
-            int ters = 0;
+        //    for (int a = start; a <= finish; a++)
+        //    {
+        //        result = Math.Abs(a - Ters(a));
+        //        if (result % k == 0)
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //    return count;
+        //}
+        //public static int Ters(int sayi)
+        //{
+        //    int ters = 0;
 
-            while (sayi>0)
+        //    while (sayi > 0)
+        //    {
+        //        int basamak = sayi % 10;
+        //        ters = ters * 10 + basamak;
+        //        sayi = sayi / 10;
+        //    }
+        //    return ters;
+        //} 
+        #endregion
+
+        public static int nonDivisibleSubset(int k, List<int> s)
+        {
+            if (k < 2) return 1;
+            int[] arr = new int[k];
+            for (int i = 0; i < s.Count(); i++)
             {
-                int basamak = sayi % 10;
-                ters = ters * 10 + basamak;
-                sayi = sayi / 10;
+                arr[s[i] % k]++;
             }
-            return ters;
+            int c = 0;
+            for (int i = 1; i < k / 2; i++)
+            {
+                c += Math.Max(arr[i], arr[k - i]);
+            }
+            if (arr[0] > 1) 
+                c++;
+            if (k % 2 == 0 && arr[k / 2] > 0)
+            {
+                c++;
+            }
+            if (k % 2 != 0)
+            {
+                c += Math.Max(arr[k / 2], arr[k - k / 2]);
+            }
+            return c;
         }
 
         //public static List<int> icecreamParlor(int m, List<int> arr)
---
+++ HackerRank.Sample03/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Sample03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Girilen Sayıyı 2'lik tabana çevirip peş peşe gelen maksimum 1'lerin sayısını bulma
+            //int n = int.Parse(Console.ReadLine());
+            //List<int> list = new List<int>();
+            //int count = 0;
+            //List<int> countList = new List<int>();
+
+            //while (n > 0)
+            //{
+            //    int a = n % 2;
+            //    n = n / 2;
+            //    list.Add(a);
+            //}
+
+            //foreach (int a in list)
+            //{
+            //    if (a == 1)
+            //    {
+            //        count += a;
+            //        countList.Add(count);
+            //    }
+            //    else if (a == 0)
+            //    {
+            //        count = 0;
+            //    }
+            //}
+            //Console.WriteLine(countList.Max()); 
+            #endregion
+
+            #region Counting Sort 1
+
+            //List<int> arr = new List<int>() //{1,1,3,2,1 };
+            //{ 63 ,25, 73 ,1,98, 73, 56, 84, 86, 57, 16, 83, 8 ,25, 81, 56, 9, 53, 98, 67, 99 ,12, 83 ,89, 80, 91 ,39 ,86, 76, 85, 74, 39, 25, 90, 59, 10, 94 ,32 ,44, 3 ,89,30 ,27 ,79, 46, 96, 27, 32, 18 ,21, 92, 69, 81 ,40 ,40, 34, 68, 78, 24, 87, 42 ,69, 23 ,41, 78, 22 ,6 ,90, 99, 89 ,50, 30, 20, 1 ,43, 3,70, 95, 33, 46, 44, 9, 69, 48, 33, 60, 65, 16, 82 ,67 ,61, 32, 21, 79 ,75 ,75 ,13, 87 ,70, 33 };
+
+            //int[] count = new int[100];
+
+            //foreach(int i in arr)
+            //{
+            //    count[i] += 1;
+            //}
+
+            //foreach(int item in count)
+            //{
+            //    Console.Write(item+" ");
+            //}
+            #endregion
+
+            #region Flipping The Matrix
+            List<List<int>> arr = new List<List<int>>();
+            List<int> list1 = new List<int>() { 112,42,83,119};
+            List<int> list2 = new List<int>() { 56,125,56,49};
+            List<int> list3 = new List<int>() { 15,78,101,43 };
+            List<int> list4 = new List<int>() { 62,98,114,108 };
+
+            arr.Add(list1);
+            arr.Add(list2);
+            arr.Add(list3);
+            arr.Add(list4);
+
+            int toplam = 0;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[0][0] < arr[arr.Count][0])
+                {
+                    //kolonu ters çevir
+                }
+                if (arr[0][0] < arr[0][arr.Count])
+                {
+                    //satırı ters çevir
+                }
+            }
+
+
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
